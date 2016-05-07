c57c10d37f1743f0862976e8c2a376f8d1fdcdce
Robert Brem <brem_robert@hotmail.com>
2016-05-07 10:56:01 +0200

    call rest service

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'home',
@@ -9,11 +11,39 @@ import {Component} from '@angular/core';
   pipes: []
 })
 export class Home {
+  players:Observable<any>;
+  createdPlayer:Observable<any>;
 
-  constructor() {}
+  constructor(private http:Http) {
+  }
 
   ngOnInit() {
+    this.players = this.getPlayers();
+  }
 
+  createPlayer(firstName:string, lastName:string) {
+    console.log('createPlayers called');
+
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+
+    var toAdd = JSON.stringify({firstName: firstName, lastName: lastName});
+    let url = `http://localhost:8080/pokertracker/resources/players`;
+    return this.http
+      .post(url, toAdd, {headers: headers})
+      .map((res) => res.json())
+      .subscribe((data:any) => this.createdPlayer = data,
+        error => console.log(error),
+        () => console.log('Player created'));
+    ;
+  }
+
+  private getPlayers() {
+    let url = `http://localhost:8080/pokertracker/resources/players`;
+    return this.http
+      .get(url)
+      .map((res) => res.json());
   }
 
 }
--- src/app/services/github.ts
+++ src/app/services/github.ts
@@ -4,26 +4,29 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class Github {
-  constructor(private http: Http) {}
+  constructor(private http:Http) {
+  }
 
-  getOrg(org: string) {
+  getOrg(org:string) {
     return this.makeRequest(`orgs/${org}`);
   }
 
-  getReposForOrg(org: string) {
+  getReposForOrg(org:string) {
     return this.makeRequest(`orgs/${org}/repos`);
   }
 
-  getRepoForOrg(org: string, repo: string) {
+  getRepoForOrg(org:string, repo:string) {
     return this.makeRequest(`repos/${org}/${repo}`);
   }
 
-  private makeRequest(path: string) {
+  private makeRequest(path:string) {
     let params = new URLSearchParams();
     params.set('per_page', '100');
 
     let url = `https://api.github.com/${ path }`;
-    return this.http.get(url, {search: params})
+    return this.http
+      .get(url, {search: params})
       .map((res) => res.json());
   }
+
 }
