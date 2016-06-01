c48c8f526ab065bb869170cd8b466f0643bd1187
Robert Brem <brem_robert@hotmail.com>
2016-06-01 15:33:22 +0200

    refactored headers

--- src/app/components/accountPosition/accountposition-service.ts
+++ src/app/components/accountPosition/accountposition-service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Http, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/map'
-import { Observable } from 'rxjs/Observable';
-import { Balance } from './../player/balance';
+import {Observable} from 'rxjs/Observable';
+import {Balance} from './../player/balance';
 import {AccountPosition} from "./accountPosition";
 import {TimeEntry} from "./timeentry";
 import {HistoryEntry} from "./historyentry";
@@ -14,7 +14,7 @@ export class AccountPositionService {
 
   constructor(private http:Http, private kc:KeycloakService) {
     let baseUrl:string = process.env.POKERTRACKER_URL;
-    if( baseUrl == undefined) {
+    if (baseUrl == undefined) {
       baseUrl = 'http://localhost:8080/pokertracker/';
     }
     let api:string = 'resources/';
@@ -29,57 +29,49 @@ export class AccountPositionService {
       headers.append('Accept', 'application/json');
       headers.append('Authorization', 'Bearer ' + token);
       return this.http
-        .post(this.playersUrl + playerId + '/accountpositions/', toAdd, new RequestOptions({ headers: headers }))
+        .post(this.playersUrl + playerId + '/accountpositions/', toAdd, new RequestOptions({headers: headers}))
         .map(res => res.json());
     });
   }
 
   public getBalance = (playerId:number):Promise<Observable<Balance>> => {
-      return this.kc.getToken().then(token => {
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        headers.append('Authorization', 'Bearer ' + token);
-        return this.http
-          .get(this.playersUrl + playerId + '/balance', new RequestOptions({ headers: headers }))
-          .map(res => res.json());
-      });
+    return this.kc.getToken().then(token => {
+      return this.http
+        .get(this.playersUrl + playerId + '/balance', new RequestOptions({headers: this.getHeaders(token)}))
+        .map(res => res.json());
+    });
   }
 
   public getAccountPositions = (playerId:number):Promise<Observable<AccountPosition[]>> => {
-      return this.kc.getToken().then(token => {
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        headers.append('Authorization', 'Bearer ' + token);
-        return this.http
-          .get(this.playersUrl + playerId + '/accountpositions/', new RequestOptions({ headers: headers }))
-          .map(res => res.json());
-      });
+    return this.kc.getToken().then(token => {
+      return this.http
+        .get(this.playersUrl + playerId + '/accountpositions/', new RequestOptions({headers: this.getHeaders(token)}))
+        .map(res => res.json());
+    });
   }
 
   public getAccountHistoryForPlayer = (playerId:number):Promise<Observable<TimeEntry[]>> => {
-      return this.kc.getToken().then(token => {
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        headers.append('Authorization', 'Bearer ' + token);
-        return this.http
-          .get(this.playersUrl + playerId + '/accounthistory/' + '?summedUp=true', new RequestOptions({ headers: headers }))
-          .map(res => res.json());
-      });
+    return this.kc.getToken().then(token => {
+      return this.http
+        .get(this.playersUrl + playerId + '/accounthistory/' + '?summedUp=true', new RequestOptions({headers: this.getHeaders(token)}))
+        .map(res => res.json());
+    });
   }
 
   public getAccountHistory = (timeUnit:string, maxEntries:number):Promise<Observable<HistoryEntry[]>> => {
-      return this.kc.getToken().then(token => {
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        headers.append('Authorization', 'Bearer ' + token);
-        return this.http
-          .get(this.playersUrl + 'accounthistory/' + '?summedUp=true&timeUnit=' + timeUnit + '&maxEntries=' + maxEntries, new RequestOptions({ headers: headers }))
-          .map(res => res.json());
-      });
+    return this.kc.getToken().then(token => {
+      return this.http
+        .get(this.playersUrl + 'accounthistory/' + '?summedUp=true&timeUnit=' + timeUnit + '&maxEntries=' + maxEntries, new RequestOptions({headers: this.getHeaders(token)}))
+        .map(res => res.json());
+    });
+  }
+
+  private getHeaders(token) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    headers.append('Authorization', 'Bearer ' + token);
+    return headers;
   }
 
 }
